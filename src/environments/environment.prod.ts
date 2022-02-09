1428c63e877c3ed89e5652c22c0ccfd562ce322b
codyhofmann@gmail.com <Technology@98>
2022-02-08 22:06:50 -0600

    Fixed prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  directusBaseURL: 'https://admin.intentionalhealthjourney.com/',
+  directusBaseURL: 'https://admin.intentionalhealthjourney.com',
   emailApiUrl: 'https://le8iz57ayk.execute-api.us-east-1.amazonaws.com/send-email'
 };
