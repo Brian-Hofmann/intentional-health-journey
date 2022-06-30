deca1b151eee2b80b3d7e810f91d0f5d275067d7
Brian Hofmann <codyhofmann@gmail.com>
2022-06-29 21:08:53 -0500

    Added live paypal client ID

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   directusBaseURL: 'https://admin.intentionalhealthjourney.com',
   emailApiUrl: 'https://le8iz57ayk.execute-api.us-east-1.amazonaws.com/send-email',
-  paypalClientId: 'AaNZeKX3apTHLMqcTO_aPLSwarRUpUlcW8euthD5RuVWmqqve1iGViZxypXw53MdpIIdlRVGXi7J347g'
+  paypalClientId: 'AWXc_O4CnUMWeVMdYaZoB5kBxKGy5iTz-O8lqCeGyyAvg4GDjCRtVaenLfilZkn55VdF6LGm_Z4xJ4W1'
 };
